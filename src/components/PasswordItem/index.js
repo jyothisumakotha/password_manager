bdb30d0ccb7e3e1baa4c67813fbc45224f499747
jyothisumakotha <111627972+jyothisumakotha@users.noreply.github.com>
2022-11-09 23:03:28 +0530

    Update index.js

--- src/components/PasswordItem/index.js
+++ src/components/PasswordItem/index.js
@@ -30,7 +30,7 @@ const PasswordItem = props => {
           />
         )}
       </div>
-      <button type="button" className="delete-btn" onClick={onDeleteBtn}>
+      <button type="button" className="delete-btn" onClick={onDeleteBtn} testid="delete">
         <img
           src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png "
           alt="delete"
